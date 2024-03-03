f91cf603b730dab93f5aca5e396c0ab2f71eca7e
Eduardo Santiago <eduardosantiago_@hotmail.com>
2024-03-03 16:44:42 -0300

    feat: hero component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="container">
+        <Hero />
+      </div>
     </>
   );
 };
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <div className="hero-content">
+        <h2>Building digital experiences that inspire</h2>
+        <p>
+          Passionate Frontend Developer | Transforming Ideas into Seamless and
+          Visually Stunning Web Solutions
+        </p>
+      </div>
+
+      <div className="hero-img">
+        <img src="./assets/images/imgperfil.jpg" alt="imagem de perfil" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
