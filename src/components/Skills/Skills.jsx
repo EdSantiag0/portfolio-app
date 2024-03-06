1a943d5d90b412f374335c5745bad7d30cc9014c
Eduardo Santiago <eduardosantiago_@hotmail.com>
2024-03-06 11:24:31 -0300

    feat: skillsinfocard component created

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skills.css";
 import { SKILLS } from "../../utils/data";
 import SkillCard from "./SkillCard/SkillCard";
+import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard";
 
 const Skills = () => {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+  const handleSelectSkill = (data) => {
+    setSelectedSkill(data);
+  };
+
   return (
     <section className="skills-container">
       <h5>Technical Proficiency</h5>
@@ -15,11 +22,20 @@ const Skills = () => {
               key={item.title}
               iconUrl={item.icon}
               title={item.title}
+              isActive={selectedSkill.title === item.title}
+              onClick={() => {
+                handleSelectSkill(item);
+              }}
             />
           ))}
         </div>
 
-        <div className="skills-info"></div>
+        <div className="skills-info">
+          <SkillsInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
       </div>
     </section>
   );
