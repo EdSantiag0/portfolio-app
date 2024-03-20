5027143b08aba29d92da6baf3e2816921fc561d4
Eduardo Santiago <eduardosantiago_@hotmail.com>
2024-03-20 13:43:01 -0300

    feat: fix icon problem ProjectCard

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -4,13 +4,16 @@ import "./ProjectCard.css";
 const ProjectCard = ({ details }) => {
   return (
     <div className="project-card-container">
-      <img src={details.img} alt="Imagem do projeto" />
+      <div className="project-card-img">
+        <img src={details.img} alt="Imagem do projeto" />
+      </div>
+
       <div className="project-card-icons">
-        <a href={details.link1} target="_blank" rel="noopener noreferrer">
-          <img src={details.icon1} alt={details.text1} />
+        <a href={details.linkgit} target="_blank" rel="noopener noreferrer">
+          <img src={details.icongit} alt="imgem icone" />
         </a>
-        <a href={details.link2} target="_blank" rel="noopener noreferrer">
-          <img src={details.icon2} alt={details.text2} />
+        <a href={details.linkproject} target="_blank" rel="noopener noreferrer">
+          <img src={details.iconlink} alt="imagem link" />
         </a>
       </div>
 
